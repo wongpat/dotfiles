6e28ef099e12e995549f6b09a1e1f42150b4c5d5
Patrick Wong <pwong@peak6.com>
2016-04-26 14:49:12 -0500

    Support for conda environments

--- zsh/virtualenv/zshrc.sh
+++ zsh/virtualenv/zshrc.sh
@@ -2,9 +2,14 @@
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 function venv_prompt {
     local venv=""
-    if [ -n "$VIRTUAL_ENV" ]; then
-        name=$(basename "$VIRTUAL_ENV")
-        venv="(%{$fg[cyan]%}$name%{$reset_color%}) "
+    local whatvenv="${VIRTUAL_ENV:-$CONDA_ENV_PATH}"
+    if [ -n "$whatvenv" ]; then
+        name=$(basename "$whatvenv")
+        if [ -n "$CONDA_ENV_PATH" ]; then
+            venv="(%{$fg[red]%}C%{$reset_color%}|%{$fg[cyan]%}$name%{$reset_color%}) "
+        else
+            venv="(%{$fg[magenta]%}V%{$reset_color%}|%{$fg[cyan]%}$name%{$reset_color%}) "
+        fi
     fi
     echo $venv
 }
